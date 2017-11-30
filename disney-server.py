ed21cc445707903a58ed9623d65a7ed759e56cee
月岡祐太 <y-tsukioka@biglobe.co.jp>
2017-11-30 14:19:08 +0900

    できた

--- disney-server.py
+++ disney-server.py
@@ -1,41 +1,53 @@
 # coding=utf-8
-import re
-from flask import Flask
+import json
+from flask import Flask, jsonify
 from urllib.request import Request, urlopen
 from bs4 import BeautifulSoup
 
 app = Flask(__name__)
-
+app.config['JSON_AS_ASCII'] = False
 
 url = 'http://info.tokyodisneyresort.jp/schedule/stop/stop_list.html'
 req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})#ユーザーエージェント偽装
 response = urlopen(req)
 html = response.read()
 soup = BeautifulSoup(html, "html.parser")
-land = soup.find("section", class_="tdl col")
-sea = soup.find("section", class_="tds col")
 
 
-@app.route('/disney-api', methods=['GET'])
-def get_json():
-    return get_land_elements()
+@app.route('/disney-api.json', methods=['GET'])
+def get_disney_json():
+    land_list = get_json(soup.find("section", class_="tdl col"), "land")
+    sea_list = get_json(soup.find("section", class_="tds col"), "sea")
+    d_dict = {"attractions": land_list+sea_list}
+    return json.dumps(d_dict, ensure_ascii=False, indent=4)
 
 
-def get_land_elements():
-    attraction = land.findAll("dd")[0]
-    show = land.findAll("dd")[1]
-    character = land.findAll("dd")[2]
-    shop = land.findAll("dd")[3]
-    restaurant = land.findAll("dd")[4]
-    service = land.findAll("dd")[5]
-    row = []
-    for e in attraction.find(["a","p"]):
-        row.append(e.get)
-    return row
+def get_json(s_list, place):
+    attraction = get_element(s_list.findAll("dd")[0], place, "attraction")
+    show = get_element(s_list.findAll("dd")[1], place, "show")
+    character = get_element(s_list.findAll("dd")[2], place, "character")
+    shop = get_element(s_list.findAll("dd")[3], place, "shop")
+    restaurant = get_element(s_list.findAll("dd")[4], place, "restaurant")
+    service = get_element(s_list.findAll("dd")[5], place, "service")
+    e_list = attraction+show+character+shop+restaurant+service
+
+    return e_list
 
 
-def get_sea_elements():
-    return 'sea'
+def get_element(e_list, place, typ):
+    json_arr = []
+    for a in e_list.findAll("li"):
+        title = a.find(["a", "p"]).get_text()
+        span = a.find("span").get_text().split("-")
+        if len(span) == 1:
+            start = span[0]
+            end = span[0]
+        else:
+            start = span[0]
+            end = span[1]
+        json_arr.append({"place": place, "type": typ, "title": title, "start": start, "end": end})
+
+    return json_arr
 
 
 if __name__ == '__main__':
